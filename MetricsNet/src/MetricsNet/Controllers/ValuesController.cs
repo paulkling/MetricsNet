4b63c37a8c2c8d8d80fb4ce9fea303c80cef5a54
paul.kling <paul.kling@protolabs.com>
2016-11-09 16:46:33 -0600

    adding one simple counter for metrics

--- MetricsNet/src/MetricsNet/Controllers/ValuesController.cs
+++ MetricsNet/src/MetricsNet/Controllers/ValuesController.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Metrics;
 
 namespace MetricsNet.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+
+        private readonly Counter counter = Metric.Counter("GetCounter", Unit.Requests);
+
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
+            counter.Increment();
             return new string[] { "value1", "value2" };
         }
 
